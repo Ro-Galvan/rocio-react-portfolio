328fba537d278b7bb6cd9a5ac81278c6fdef7d23
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2022-04-27 21:03:25 -0400

    added footer to the page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,19 @@
 
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
+import Footer from "./components/pages/Footer";
 
 import './App.css';
 
-const App = () => <PortfolioContainer />;
+// const App = () => <PortfolioContainer />;
+
+const App = () => {
+  return (
+    <>
+    <PortfolioContainer />,
+    <Footer />
+    </> 
+  );
+}
 
 export default App;
