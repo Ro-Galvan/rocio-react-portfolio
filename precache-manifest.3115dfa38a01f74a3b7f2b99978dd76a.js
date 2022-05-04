92603833739e1a61916ac9d048aeb1c12a4dada9
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2022-05-03 20:08:38 -0400

    Updates

---
+++ precache-manifest.3115dfa38a01f74a3b7f2b99978dd76a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe3071a7ecacc31e0a70",
+    "url": "/rocio-react-portfolio/static/css/main.cfc8a87a.chunk.css"
+  },
+  {
+    "revision": "fe3071a7ecacc31e0a70",
+    "url": "/rocio-react-portfolio/static/js/main.fe3071a7.chunk.js"
+  },
+  {
+    "revision": "441dc7d38b20c38092ba",
+    "url": "/rocio-react-portfolio/static/css/1.8bb1d9ac.chunk.css"
+  },
+  {
+    "revision": "441dc7d38b20c38092ba",
+    "url": "/rocio-react-portfolio/static/js/1.441dc7d3.chunk.js"
+  },
+  {
+    "revision": "c972130d26500b998458",
+    "url": "/rocio-react-portfolio/static/js/runtime~main.c972130d.js"
+  },
+  {
+    "revision": "c35baaf50102636cf8d522711abdfc8f",
+    "url": "/rocio-react-portfolio/index.html"
+  }
+];
